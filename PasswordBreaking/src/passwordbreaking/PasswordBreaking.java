9a7e58a55b5f253c37d619e750d3517f4cd5b32c
azzeozel <99212853+azzeozel@users.noreply.github.com>
2022-12-29 14:02:56 +0300

    Update PasswordBreaking.java
    Numbers.txt is read, possible password combinations are separated as valid-invalid according to test cases.

--- PasswordBreaking/src/passwordbreaking/PasswordBreaking.java
+++ PasswordBreaking/src/passwordbreaking/PasswordBreaking.java
@@ -2,6 +2,9 @@ package passwordbreaking;
 
 
 import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,48 +13,60 @@ public class PasswordBreaking {
 
 
 	public static void main(String[] args) {
+		File fp = new File("numbers.txt");
 		
-	    String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};	 
-            String password = "423";
-	    int[] pass = new int[password.length()];
 		
-	   
-	    if(numericTest(password) && lenghtTest(password) ) {
-	    	
-	    	String [] passs  = password.split("");
+		String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};	 
+		String password;
+		
 		try {
-			for(int j = 0; j < passs.length; j++) {
-			pass[j] = Integer.parseInt(passs[j]);
-             }
-		}catch(NumberFormatException n) {
-			System.out.println("You did not enter password!!");
-		}
-	    
-
-	    String passwordByWritten = "";
-	    
-	    for(int i = 0; i < pass.length; i++ ) {
-	    	passwordByWritten += digits[pass[i]];
-	    }
-	    if(letterTest(passwordByWritten)) {
-	    	System.out.println(passwordByWritten);
-	    }
+			Scanner scan = new Scanner(fp);
+			while(scan.hasNext()) {
+		    password = scan.nextLine();
+ 
+		    int[] pass = new int[password.length()];
+	 
+		    if(numericTest(password) && lenghtTest(password) ) {
+		    	
+		    	String [] passs  = password.split("");
+			try {
+				for(int j = 0; j < passs.length; j++) {
+				pass[j] = Integer.parseInt(passs[j]);
+	             }
+			}catch(NumberFormatException n) {
+				System.out.println("You did not enter password!!");
+			}
+			}
+		   	    
+		    String passwordByWritten = "";
 		    
-            
-	    }
-	    
-       
-        }
+		    for(int i = 0; i < pass.length; i++ ) {
+		    	passwordByWritten += digits[pass[i]];
+		    }
+		    if(letterTest(passwordByWritten)) {
+		    	System.out.println(passwordByWritten);
+		    }
+			}
+		}catch(FileNotFoundException f) {
+			f.printStackTrace();
+		}
+		
+	     
+	    }	    
+        
   
           public static boolean letterTest(String passwordByWritten){
           ArrayList<AbstractMap.SimpleEntry<String,Integer>> listOfPair = new ArrayList<>();
           AbstractMap.SimpleEntry<String,Integer> apm;
-        int total = 0;
+         int total;
 	    String passChars[];
 	    String newString = "";
 	    for(int k = 0; k < passwordByWritten.length(); k++) {
-	    	newString = passwordByWritten.substring(0, k);
+	    	total = 0;
+	    	newString = passwordByWritten.substring(0, k+1);
+	    	//System.out.println(newString);
 	    	for(int l = 0; l < newString.length(); l ++) {
+	    		
 	    		passChars = newString.split("");
 	    	if(passChars[l].matches("a|e|i|o|u")) {
 	    		
@@ -63,24 +78,15 @@ public class PasswordBreaking {
 	    		total--;
 	    		//listOfPair.add(apm = new AbstractMap.SimpleEntry<>(passChars[k],-1));
 	    	}
-	    	
 	    	}
-	    	total = 0;
 	    	if(total > 0) {
 	    	System.out.println("Number of vowels can not be more than number of consonant!! Invalid");
-	    	return false;
-	    	
-	    	
-	    }else {
-	    	return true;
-	    	//System.out.println(listOfPair.get(k).getValue() +"  ");
-	    }
-	    	
-	    			
-	    }
-             
-	  return true;
+            return false;
 	    
+	    	}
+	    }
+	    return true;
+
           }
           
           public static boolean lenghtTest(String password){
