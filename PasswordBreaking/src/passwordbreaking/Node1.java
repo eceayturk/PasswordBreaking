23d7e6d38b95c9d7f5f2fa7761a7915dcee7dcf3
urfaty <97344694+urfaty@users.noreply.github.com>
2022-12-30 13:00:06 +0300

    Create Node1.java
    Node1 class added for tree1 class

---
+++ PasswordBreaking/src/passwordbreaking/Node1.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Node1<String> {
+    private String data;
+    private List<Node1<String>> children;
+    private Node1<String> parent;
+
+    public Node1(String data) {
+        this.data = data;
+        this.children = new ArrayList<Node1<String>>();
+    }
+
+
+    public Node1(Node1<String> node) {
+        this.data = node.getData();
+        children = new ArrayList<Node1<String>>();
+    }
+
+    public void addChild(Node1<String> child) {
+        child.setParent(this);
+        children.add(child);
+    }
+
+
+    public List<Node1<String>> getChildren() {
+        return this.children;
+    }
+
+
+    public String getData() {
+        return this.data;
+    }
+
+
+    public void setParent(Node1<String> parent) {
+        this.parent = parent;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (null == obj)
+            return false;
+
+        if (obj instanceof Node1) {
+            if (((Node1<?>) obj).getData().equals(this.data))
+                return true;
+        }
+
+        return false;
+    }
+
+}
