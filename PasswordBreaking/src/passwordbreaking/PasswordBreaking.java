825022d5bd97013a15031fb4fdd75913a7e36156
Ece Aytürk <75135337+eceayturk@users.noreply.github.com>
2023-01-05 15:24:44 +0300

    Update PasswordBreaking.java
    Necessary comments has been added.

--- PasswordBreaking/src/passwordbreaking/PasswordBreaking.java
+++ PasswordBreaking/src/passwordbreaking/PasswordBreaking.java
@@ -16,59 +16,57 @@ public class PasswordBreaking {
 		File fp = new File("numbers.txt");
                 
 	
-		String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};	 
-		String password;
-                int[] newPass;
+		String[] digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //necessary digit written format.
+		String password; //for testing, all possible 0-5 digit passwords  
+                int[] newPass; //for user password.
                 String[] newPas ={} ;
                 boolean flag = true;
-                String newWritten = "";
+                String newWritten = ""; //user password's written format.
                 Scanner scann = new Scanner(System.in);
                 System.out.println("Please enter a password: ");
 		String enteredPass = scann.next();
-                System.out.println("\nAdding, scanning and breaking may take some time.\nPLEASE WAIT UNTIL THE RESULTS APPEAR!\n");
+                System.out.println("\nAdding, scanning and breaking operations may take some time.\nPLEASE WAIT UNTIL THE RESULTS APPEAR!\n");
                 
                 
 		try {
 			Scanner scan = new Scanner(fp);   //read the file
 		while(scan.hasNext()) {
-		        password = scan.nextLine();
+		        password = scan.nextLine(); 
  
-		    int[] pass = new int[password.length()];
-                   newPass = new int[enteredPass.length()];
+		    int[] pass = new int[password.length()]; // for each password digit locate cell.(for passwords in txt)
+                   newPass = new int[enteredPass.length()]; //for each user's password digit locate cell.
 	 
 		    if(numericTest(password) && lenghtTest(password) ) {  //checking whether the password is digit and smaller than 20 lenght
-		    	newPas = enteredPass.split("");
-		    	String [] passs  = password.split("");
+		    	newPas = enteredPass.split(""); //for example 1234 --> split version [1] [2] [3] [4] ---> user's password
+		    	String [] passs  = password.split(""); //--> txt password.
 			
 				for(int j = 0; j < passs.length; j++) {
-				pass[j] = Integer.parseInt(passs[j]);
+				pass[j] = Integer.parseInt(passs[j]); //parsing string values to int for txt passwords.
 	             }
                    
 			}
                 try{
 		   for(int k = 0; k < newPas.length; k++){
-                   newPass[k] = Integer.parseInt(newPas[k]);
+                   newPass[k] = Integer.parseInt(newPas[k]); //parsing string values to int for user's password.
                    }
-                }catch(NumberFormatException e){
-                   // System.out.println("Your password did not pass numeric and length test.");
+                }catch(NumberFormatException e){ //to handle with with passwords containing letters or characters.
                     break;
                 }
-		   String passwordByWritten = "";
-                   newWritten = "";
+		   String passwordByWritten = ""; //written format of txt password.
+                   newWritten = ""; //written format of user's password.
 		    
 		    for(int i = 0; i < pass.length; i++ ) {
-		    	passwordByWritten += digits[pass[i]];
+		    	passwordByWritten += digits[pass[i]]; //adding "text value for digits" into written format.
+                        // like, digits[0] = "zero"
 		    }
                     for(int i = 0; i < newPass.length; i ++){
-                        newWritten += digits[newPass[i]];
+                        newWritten += digits[newPass[i]]; // same with txt passwords for user's password.
                     }
-                    flag = addTree(passwordByWritten,newWritten,enteredPass,flag);
+                    flag = addTree(passwordByWritten,newWritten,enteredPass,flag); //keeping flag change while adding values to tree.
                   
                     
                 }
                     if(flag == true ){
-                       //System.out.println("Password is valid"); 
-                      
                        if(letterTest(newWritten)){
                            if(numericTest(enteredPass)){
                                if(lenghtTest(enteredPass)){
@@ -108,19 +106,18 @@ public class PasswordBreaking {
 	    }	    
         
           public static boolean addTree(String passwordByWritten,String newWritten,String enteredPass, boolean flag){
-             
-              if(!letterTest(passwordByWritten)) { //checking 3rd case (vowel-consonants)
-		    	//System.out.println(passwordByWritten);
-                        
-                         Node1<String> nodes = new Node1<String>(passwordByWritten);    
+             //this method includes tree implementations and with the operations it changes to flag value. 
+              if(!letterTest(passwordByWritten)) { 
+                         Node1<String> nodes = new Node1<String>(passwordByWritten); //create a node with passwordByWritten.
                          
-                         Tree1<String> tree1 = new Tree1<String>(nodes);
+                         Tree1<String> tree1 = new Tree1<String>(nodes); // create tree with node.
                          //List<Node1<String>> childs = nodes.getChildren();
-                          while(tree1.exists(newWritten)){
+                          while(tree1.exists(newWritten)){ //if syntax found in trie, than that means password invalid. 
+                              //so it takes invalid value from tree and writes an invalid massage.
                             System.out.println("Invalid password!!: "+ enteredPass+
                                      "\nYour password is in m-ary trie --> "+ ": "+nodes.getData());
                             
-                            flag = false;
+                            flag = false; //turn of flag as false.
                             break;
                            
                         }
